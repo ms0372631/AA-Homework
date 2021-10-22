56007b1df448bba87bf77acc154167dadc4967f9
Alan Chiu <ms0372631@gmail.com>
2021-10-22 07:13:22 -0700

    drew a smiley face

--- W8/D5/canvas_practice/practice.js
+++ W8/D5/canvas_practice/practice.js
@@ -16,12 +16,12 @@ document.addEventListener("DOMContentLoaded", function(){
     ctx.fill();
 
     ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
-    ctx.moveTo(65, 65);
-    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
-    ctx.moveTo(95, 65);
-    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.arc(275, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(310, 75);
+    ctx.arc(275, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(265, 65);
+    ctx.arc(260, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(295, 65);
+    ctx.arc(290, 65, 5, 0, Math.PI * 2, true);  // Right eye
     ctx.stroke();
 });
